ef33005802eb43b2d0b314d4168d763c6a7a4b45
Everton Roberto Oliveira <eroberto@everis.com>
2019-05-29 22:17:39 -0300

    alterado a porta

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     ENV: process.env.NODE_ENV,
     VERSION: process.env.APP_VERSION,
     DB_CONNECTION: getEnv(process.env.NODE_ENV),
-    PORT: getPort(process.env.NODE_ENV),
+    PORT: process.env.PORT,
     KEY: process.env.SECRET,
 };
 
@@ -17,16 +17,5 @@ function getEnv(env) {
             return process.env.DATABASE_URL_PRODUCTION;
         default:
             return process.env.DATABASE_URL_DEVELOPMENT;
-    }
-}
-
-function getPort(env) {
-    switch (env) {
-        case 'test':
-            return process.env.PORT_TEST;
-        case 'production':
-            return process.env.PORT_PRODUCTION;
-        default:
-            return process.env.PORT_DEVELOPMENT;
     }
 }
